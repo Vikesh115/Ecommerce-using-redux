1ac4b0d8efd56c3f34220e4d3946087aa4fcbc17
Vikesh raut <Vikeshraut952@gmail.com>
2024-12-31 18:06:08 +0530

    lazy loading test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, lazy, Suspense } from 'react'
 import './App.css'
 import { Route, Routes, Navigate } from 'react-router-dom'
 import Cart from './pages/Cart/Cart'
 import Navbar from './components/Navbar/Navbar'
 import Wishlist from './pages/Wishlist/Wishlist'
 import ProductDetails from './pages/ViewProductDetails/ProductDetails'
-import Dashboard from './pages/Products/Dashboard'
+const Dashboard = lazy(() => import('./pages/Products/Dashboard') );
+// import Dashboard from './pages/Products/Dashboard'
 import Signup from './pages/Signup/Signup'
 import Login from './pages/Login/Login'
 
@@ -23,7 +24,12 @@ function App() {
         (<>
           <Navbar handleLogout={handleLogout} />
           <Routes>
-            <Route path='/' element={<Dashboard />} />
+          <Route path='/' element={<Suspense
+                fallback={<div className='flex justify-center item-center text-2xl text-cyan-900 font-bold'>DASHBOARD are loading please wait...</div>}
+            >
+                <Dashboard />
+            </Suspense>} />
+            {/* <Route path='/' element={<Dashboard />} /> */}
             <Route path="/viewproduct/:id" element={<ProductDetails />} />
             <Route path='/cart' element={<Cart />} />
             <Route path='/wishlist' element={<Wishlist />} />
